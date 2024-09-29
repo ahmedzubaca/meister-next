1594747a53ba40a3d7fce764f21533316a6e4c2d
ahmedzubaca <ahmed.zubaca@gmail.com>
2024-09-29 18:29:36 +0200

    seo1

--- app/about/page.js
+++ app/about/page.js
@@ -5,7 +5,10 @@ import MoveDownUpAnimation from '@/lib/animations/moveDownAnimation';
 import styles from './about.module.css';
 
 export const metadata = {
-  title: 'O nama'  
+  title: 'O nama',
+  description: 'Meister d.o.o. je primarno orjentisan na brzu i kvalitetnu izgradnju' + 
+  'ali jednako kvalitetno nudi rješenja za arhitektonske i statičke projekte' + 
+  'kao i profesionalnu pomoć u prodaji nekretnina.'
 };
 
 export default function About() {
--- app/projects/page.js
+++ app/projects/page.js
@@ -1,6 +1,12 @@
 import AllProjects from "@/components/Projects/AllProjects";
 import { getProjects } from "@/lib/dataFromDb/projectsFromDb";
 
+export const metadata = {
+  title: 'Projekti',
+  description: 'Uspješno realizujemo projekte od relativno jednostavnih do najzahtjevnijih.' + 
+  'Izgradili smo više od 250 000 metara kvadratnih stambeno poslovnih prostora.'  
+};
+
 export default function ProjectsDisplay() {
 
   const projects = getProjects('cover');  
