c80613658d757b1413585803a3d9f2a58f719e45
ahmedzubaca <ahmed.zubaca@gmail.com>
2024-10-03 13:53:43 +0200

    ignorecase false

---
+++ components/projects/ProjectDetails.js
@@ -0,0 +1,118 @@
+'use client';
+import Link from 'next/link';
+import { useEffect, useState, useRef } from 'react';
+import { TbArrowBadgeLeftFilled, TbArrowBadgeRightFilled } from "react-icons/tb";
+import { imageVideoRender } from './helpers/imageVideoRender';
+import styles from './projectsDetails.module.css';
+
+export default function ProjectsDetails({projectDetails}) {
+
+  const [ slideIndex, setSlideIndex ] = useState(0);
+  const [ sliderMob, setSliderMob] = useState(false);
+  const [direction, setDirection] = useState(0);
+  const touchStartX = useRef(null);
+  
+  const handlePreviousArrow = () => {
+    setDirection(-1);
+    if(slideIndex > 0) {
+      setSlideIndex(prev => prev - 1);
+    }      
+  }
+
+  const handleNextArrow = () => {
+    setDirection(1);
+    if(slideIndex < projectDetails.length -1) {
+      setSlideIndex(prev => prev + 1);
+    }   
+  }
+
+  const handleCircleClick = (number) => {
+    if(number > slideIndex){
+      setDirection(1);
+    } 
+    if (number < slideIndex) {
+      setDirection(-1);
+    }
+    setSlideIndex(number);
+    if(number === projectDetails.length -1 || number === 0) setSliderMob(prev => !prev);
+  } 
+
+  const handleTouchStart = (e) => {
+    touchStartX.current = e.touches[0].clientX;    
+  };
+
+  const handleTouchEnd = (e) => {
+    if (touchStartX.current === null) return;
+    const touchEndX = e.changedTouches[0].clientX;
+    const diff = touchEndX - touchStartX.current;
+
+    if (diff > 50 ) {
+      setSliderMob(prev => !prev);
+      handlePreviousArrow();      
+    }
+    if (diff < -50 ) {
+      setSliderMob(prev => !prev)
+      handleNextArrow();
+    }
+    touchStartX.current = null;    
+  };  
+
+  useEffect(() => {
+    window.addEventListener('touchstart', handleTouchStart);
+    window.addEventListener('touchend', handleTouchEnd);
+       
+    return () => {      
+      window.removeEventListener('touchstart', handleTouchStart);
+      window.removeEventListener('touchend', handleTouchEnd);
+    };    
+  }, [sliderMob]); // eslint-disable-line react-hooks/exhaustive-deps
+  
+  return ( 
+    <div className={styles.backgroundProjects}>
+      <div className={styles.pageContainer}>
+        <div className={styles.buttonContainer}>
+          <Link href={'/projects'} >
+            <button className={styles.backButton}> 
+              NAZAD
+            </button>
+          </Link>
+        </div>
+        <div className={styles.projectCardContainer}>
+          <div className={styles.projectInfo}>
+                <p> Lokacija: {projectDetails[0].location} </p>
+                <p> Investitor: {projectDetails[0].investor} </p>
+                <p> Površina: {projectDetails[0].size}  m<sup>2</sup></p>
+          </div>
+          <div className={styles.imageAndArrowsContainer}>
+            <div className={styles.arrowContainer}>
+              <TbArrowBadgeLeftFilled className={`${slideIndex === 0 ? styles.arrowHidden : styles.arrow}`}
+                  onClick={handlePreviousArrow} 
+              />
+            </div>
+            <div className={styles.imageContainer}> 
+              {                
+                imageVideoRender(projectDetails[slideIndex].imageUrl, styles.image, direction, slideIndex)                  
+              }      
+            </div>
+            <div className={styles.arrowContainer}> 
+              <TbArrowBadgeRightFilled className={`${slideIndex === projectDetails.length - 1 ? styles.arrowHidden : styles.arrow}`} 
+                  onClick={handleNextArrow}
+              />
+            </div>            
+          </div>
+          <div className={styles.circlesContainer} >
+            {
+              projectDetails.map((slide, index) => (
+                <span key={index} 
+                className={`${styles.circle} ${slideIndex === index ? styles.activeCircle : null}`}
+                    onClick={() => handleCircleClick(index)} 
+                > 
+                </span>
+              ))
+            } 
+          </div>             
+        </div>
+      </div>      
+    </div>
+  );
+}
---
+++ components/projects/helpers/projectsDataImportedImages.js
@@ -0,0 +1,130 @@
+// import p1_cover from "../images/p1_fasadaPotocari/normal/p1_cover.jpg";
+// import p11 from "../images/p1_fasadaPotocari/normal/p11.jpg";
+// import p12 from "../images/p1_fasadaPotocari/normal/p12.jpg";
+// import p13 from "../images/p1_fasadaPotocari/normal/p13.jpg";
+// import p2_cover from '../images/p2_kiseljakBarFloor/normal/p2_cover.jpg';
+// import p21 from '../images/p2_kiseljakBarFloor/normal/p21.jpg';
+// import p22 from '../images/p2_kiseljakBarFloor/normal/p22.jpg';
+// import p23 from '../images/p2_kiseljakBarFloor/normal/p23.jpg';
+// import p24 from '../images/p2_kiseljakBarFloor/normal/p21.jpg';
+// import p3_cover from '../images/p3_krovPotocari/normal/p3_cover.jpg';
+// import p31 from '../images/p3_krovPotocari/normal/p31.jpg';
+// import p32 from '../images/p3_krovPotocari/normal/p32.jpg';
+// import p4_cover from '../images/p4_novaVogosca/normal/p4_cover.png';
+// import p41 from '../images/p4_novaVogosca/normal/p41.png';
+// import p42 from '../images/p4_novaVogosca/normal/p42.png';
+// import p5_cover from '../images/p5_viaPax/normal/p5_cover.jpg';
+// import p51 from '../images/p5_viaPax/normal/p51.jpg';
+// import p52 from '../images/p5_viaPax/normal/p52.jpg';
+// import p6_cover from '../images/p6_villaVerdeVogosca/normal/p6_cover.jpg';
+// import p61 from '../images/p6_villaVerdeVogosca/normal/p61.jpg';
+
+// import p1_cover_l from "../images/p1_fasadaPotocari/light/p1_cover_l.jpg";
+// import p11_l from "../images/p1_fasadaPotocari/light/p11_l.jpg";
+// import p12_l from "../images/p1_fasadaPotocari/light/p12_l.jpg";
+// import p13_l from "../images/p1_fasadaPotocari/light/P13_l.jpg";
+// //import p14_video_l  from "../images/p1_fasadaPotocari/light/p14_video_l.mp4"; 
+// import p2_cover_l from '../images/p2_kiseljakBarFloor/light/p2_cover_l.jpg';
+// import p21_l from '../images/p2_kiseljakBarFloor/light/p21_l.jpg';
+// import p22_l from '../images/p2_kiseljakBarFloor/light/p22_l.jpg';
+// import p23_l from '../images/p2_kiseljakBarFloor/light/p23_l.jpg';
+// import p24_l from '../images/p2_kiseljakBarFloor/light/p21_l.jpg';
+// import p3_cover_l from '../images/p3_krovPotocari/light/p3_cover_l.jpg';
+// import p31_l from '../images/p3_krovPotocari/light/p31_l.jpg';
+// import p32_l from '../images/p3_krovPotocari/light/p32_l.jpg';
+// //import p33_video_l from '../images/p3_krovPotocari/light/p33_video_l.mp4';
+// import p4_cover_l from '../images/p4_novaVogosca/light/p4_cover_l.jpg';
+// import p41_l from '../images/p4_novaVogosca/light/p41_l.jpg';
+// import p42_l from '../images/p4_novaVogosca/light/p42_l.jpg';
+// import p5_cover_l from '../images/p5_viaPax/light/p5_cover_l.jpg';
+// import p51_l from '../images/p5_viaPax/light/p51_l.jpg';
+// import p52_l from '../images/p5_viaPax/light/p52_l.jpg';
+// //import p53_video_l from '../images/p5_viaPax/light/p53_video_l.mp4';
+// import p6_cover_l from '../images/p6_villaVerdeVogosca/light/p6_cover_l.jpg';
+// import p61_l from '../images/p6_villaVerdeVogosca/light/p61_l.jpg';
+// //import p62_video_l from '../images/p6_villaVerdeVogosca/light/p62_video_l.mp4';
+
+export const projectsDataImportedImages = ''; 
+//   [
+//     {
+//       projectId: "p1_fasadaPotocari",
+//       title: "FASADA POTOČARI",
+//       location: "Potočari",
+//       investor: "TIKA",
+//       size: "",
+//       coverImage: p1_cover,
+//       coverImageLight: p1_cover_l, 
+//       contentImages: [p1_cover, p11, p12, p13],
+//       contentImagesLight: [p1_cover_l, p11_l, p12_l, p13_l],
+//       //video: p14_video_l,
+//       video: ''       
+//     },      
+//     {
+//       projectId: 'p2_kiseljakBarFloor',
+//       title: 'KISELJAK BAR FLOOR',
+//       location: 'Kiseljak',
+//       investor: 'BAR FLOR d.o.o.',
+//       size: '16 000',
+//       coverImage: p2_cover,
+//       coverImageLight: p2_cover_l,
+//       contentImages: [p2_cover,p21, p22, p23, p24],
+//       contentImagesLight: [p2_cover_l,p21_l, p22_l, p23_l, p24_l],
+//       video:''      
+//     },  
+//     {
+//       projectId: 'p3_krovPotocari',
+//       title: 'KROV POTOČARI',
+//       location: 'Potočari',
+//       investor: 'TIKA',
+//       size: '16 500',
+//       coverImage: p3_cover_l,
+//       coverImageLight: p3_cover,
+//       contentImages: [p3_cover, p31, p32],
+//       contentImagesLight: [p3_cover_l, p31_l, p32_l],
+//       //video: p33_video_l,
+//       video: ''             
+//     },  
+//     {
+//       projectId: 'p4_novaVogosca',
+//       title: 'NOVA VOGOŠĆA',
+//       location: 'VOGOŠĆA',
+//       investor: 'NOVA VOGOŠĆA d.o.o.',
+//       size: '20 000',
+//       coverImageLight: p4_cover_l,
+//       coverImage: p4_cover,
+//       contentImages: [p4_cover, p41, p42],
+//       contentImagesLight: [p4_cover_l, p41_l, p42_l],
+//       video: ''                
+//     },
+//     {
+//       projectId: 'p5_viaPax',
+//       title: 'VIA PAX',
+//       location: 'LOKACIJA',
+//       investor: 'INVESTITOR',
+//       size: '3 800',
+//       coverImage: p5_cover,
+//       contentImages: [p5_cover, p51, p52],
+//       coverImageLight: p5_cover_l,
+//       contentImagesLight: [p5_cover_l, p51_l, p52_l],
+//       //video: p53_video_l,
+//       video: ''      
+//     },  
+//     {
+//       projectId: 'p6_villaVerdeVogosca',
+//       title: 'VILLA VERDE',
+//       ocation: 'Vogošća',
+//       investor: 'INVESTITOR',
+//       size: '3 500',
+//       coverImage: p6_cover,
+//       contentImages: [p6_cover, p61],
+//       coverImageLight: p6_cover_l,
+//       contentImagesLight: [p6_cover_l, p61_l],
+//       //video: p62_video_l,
+//       video: ''     
+//     }    
+//   ];
+
+//   export function filteredProjectsData(projectId) {    
+//     const project =projectsData.filter((project) => project.projectId === projectId); 
+//     return project;
+//   }
